304bfd6824523c79cdebef98f6e182952c7b5021
pes2ug22cs064 <pes2ug22cs064@pes2ug22cs064.myguest.virtualbox.org>
2025-03-10 15:00:23 +0530

    Add hello.cpp

---
+++ hello.cpp
@@ -0,0 +1,10 @@
+//File: hello.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, World\n";
+  cout << "PES2UG22CS064\n";
+  cout << "I have lost my mind\n";
+  return 0;
+}
