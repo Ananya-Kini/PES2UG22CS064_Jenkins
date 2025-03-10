b6d7009947ccb526267cf51b891b77803f3f1065
pes2ug22cs064 <pes2ug22cs064@pes2ug22cs064.myguest.virtualbox.org>
2025-03-10 16:16:28 +0530

    update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 int main(void) {
-  cout << "Hello, World\n";
+  cout << "REYANSH COLLEGE OF HOTEL MANAGEMENT \n";
   cout << "PES2UG22CS064\n";
   cout << "I have lost my mind\n";
   return 0;
